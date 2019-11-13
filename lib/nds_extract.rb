7c9a85dddd58dd6cec72eaab15192c0572c99e06
Jacob Kenny <jacobkenny05@gmail.com>
2019-11-13 22:21:54 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,7 +10,11 @@ while director_index < nds.count do
   running_total = 0
   while movie_index < nds[director_index].count do
     pp "the running total for #{nds[director_index][:name]} is #{nds[director_index][:movies][movie_index][:worldwidegross]}"
-    running_total += 
+    running_total += {nds[director_index][:movies][movie_index][:worldwidegross]}
+    movie_index += 1 
+  end
+  director_index += 1 
+end 
     
     
 
